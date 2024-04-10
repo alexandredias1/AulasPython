11a1d8e5609502a5fed351455154a9c7c076777a
alexandre dias <85952047+alezinhuu@users.noreply.github.com>
2024-04-10 01:22:30 -0300

    aula_06

---
+++ aula06/exTeste.py
@@ -0,0 +1,12 @@
+pares = 102
+impares = 127
+print("Os numeros pares")
+
+while pares < 125:
+    print(pares)
+    pares += 2
+
+print("Os impares")
+while impares < 150:
+    print(impares)
+    impares += 2
